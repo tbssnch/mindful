71e1de4511874d5161b120ea0f4ca7c3a182d0db
Tyler Bussanich <tbussanich@gmail.com>
2018-03-05 17:19:55 -0800

    add second slider to entries page and slider text and styling

--- src/screens/EntriesOne.js
+++ src/screens/EntriesOne.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Slider } from 'react-native';
+import { StyleSheet, Text, View, Image, Slider, Button } from 'react-native';
 
 function EntriesOne() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HOW ARE YOU FEELING TODAY?</Text>
-      <Text style={styles.sliderText}></Text>
+      <Text style={styles.titleText}>HOW ARE YOU FEELING TODAY?</Text>
+      <Text style={styles.sliderTitleOne}>ANXIETY LEVEL</Text>
+      <View style={styles.sliderOneTextWrapper}>
+        <Text style={styles.sliderTextOne}>NONE</Text>
+        <Text style={styles.sliderTextOne}>LOW</Text>
+        <Text style={styles.sliderTextOne}>MODERATE</Text>
+        <Text style={styles.sliderTextOne}>HIGH</Text>
+        <Text style={styles.sliderTextOne}>SEVERE</Text>
+      </View>
       <Slider
-        style={{ width: 300}}
+        style={styles.sliderOne}
       />
+      <Text style={styles.sliderTitleTwo}>GENERAL MOOD</Text>
+      <Slider
+        style={styles.sliderTwo}
+      />
+
     </View>
   );
 }
@@ -19,11 +31,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#4A525A',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
+  titleText: {
     color: '#DCE1E9',
     fontSize: 20,
+    marginTop: 125,
+    marginBottom: 100,
+  },
+  sliderTitleOne: {
+    color: '#DCE1E9',
+    marginBottom: 20,
+  },
+  sliderOneTextWrapper: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  sliderTextOne: {
+    color: '#DCE1E9',
+  },
+  sliderOne: {
+    width: 300,
+    marginBottom: 100,
+  },
+  sliderTitleTwo: {
+    color: '#DCE1E9',
+  },
+  sliderTwo: {
+    width: 300,
   }
 });
 
